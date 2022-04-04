b35987a8d950b7376722af88364d2cd1964ab925
luongtx <tranxuanluong1998@gmail.com>
2022-04-04 17:11:35 +0700

    add config for swagger ui disable mongodb

--- src/main/java/com/demo/product_catalog_service/ProductCatalogServiceApplication.java
+++ src/main/java/com/demo/product_catalog_service/ProductCatalogServiceApplication.java
@@ -2,14 +2,28 @@ package com.demo.product_catalog_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableMongoRepositories
+//@EnableMongoRepositories
+@EnableSwagger2
 public class ProductCatalogServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCatalogServiceApplication.class, args);
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.demo.product_catalog_service"))
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 }
